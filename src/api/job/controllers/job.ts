287fc8efee9769fe24adf07cec43294b73a6f21b
chakri68 <chakridevireddy69@gmail.com>
2023-12-16 21:25:30 +0530

    Job create automatically creates job code

--- src/api/job/controllers/job.ts
+++ src/api/job/controllers/job.ts
@@ -2,6 +2,39 @@
  * job controller
  */
 
-import { factories } from '@strapi/strapi'
+import { factories } from "@strapi/strapi";
 
-export default factories.createCoreController('api::job.job');
+export default factories.createCoreController("api::job.job", ({ strapi }) => ({
+  async create(ctx) {
+    // Automatically generate the job code based on the last job code using the job service
+    const jobCode = await strapi.service("api::job.job").generateJobCode();
+    // Set the job code
+    ctx.request.body.jobCode = jobCode;
+
+    console.log({ jobCode });
+
+    console.log({ body: ctx.request.body });
+
+    // // Parse the CSV file using the job service
+    // const json = await strapi
+    //   .service("api::job.job")
+    //   .parseCSV(ctx.request.files.file);
+
+    // // Create the dynamic zone data
+    // const dynamicZoneData = json.map((item: any) => ({
+    //   __component: "api.job.spares",
+    //   ...item,
+    // }));
+
+    // // Set the dynamic zone data
+    // ctx.request.body.spares = dynamicZoneData;
+
+    // Create the job
+    const job = await strapi
+      .service("api::job.job")
+      .create({ data: ctx.request.body });
+
+    // Return the job
+    return job;
+  },
+}));
--- src/api/job/services/job.ts
+++ src/api/job/services/job.ts
@@ -2,6 +2,62 @@
  * job service
  */
 
-import { factories } from '@strapi/strapi';
+import { factories } from "@strapi/strapi";
 
-export default factories.createCoreService('api::job.job');
+export default factories.createCoreService("api::job.job", ({ strapi }) => ({
+  async generateJobCode() {
+    // GENERAL FORMAT :: YYYY-SE-NNN; eg: 2023-SE-001
+
+    // Get the last job code
+    const lastJob = await strapi.entityService.findMany("api::job.job", {
+      sort: {
+        jobCode: "desc",
+      },
+      limit: 1,
+    });
+
+    // Get the current year
+    const currentYear = new Date().getFullYear();
+
+    if (lastJob.length === 0) {
+      return `${currentYear}-SE-001`;
+    }
+    console.log({ lastJob: lastJob });
+
+    const jobCodeParts = lastJob[0].jobCode.split("-");
+
+    if (jobCodeParts.length !== 3) {
+      return `${currentYear}-SE-001`;
+    }
+
+    // Get the last job code year
+    const lastJobYear = parseInt(jobCodeParts[0]);
+
+    // Get the last job code serial number
+    const lastJobSerialNumber = parseInt(jobCodeParts[2]);
+
+    // Generate the new job code
+    let newJobCode = "";
+
+    if (currentYear > lastJobYear) {
+      newJobCode = `${currentYear}-SE-001`;
+    } else {
+      newJobCode = `${currentYear}-SE-${(lastJobSerialNumber + 1)
+        .toString()
+        .padStart(3, "0")}`;
+    }
+
+    return newJobCode;
+  },
+
+  async parseCSV(file: any) {
+    // Parse the CSV file
+    const csv = await strapi.plugins["upload"].services.upload.parseCsv(file);
+
+    // Convert the CSV to JSON
+    const json = await strapi.plugins["upload"].services.upload.csvToJson(csv);
+
+    // Return the parsed CSV
+    return json;
+  },
+}));
