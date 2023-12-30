95616efdb4b50d049e10f5d634e8e09895892e8c
chakri68 <chakridevireddy69@gmail.com>
2023-12-31 00:29:46 +0530

    Updated aws s3 package and updated base url format in config/plugins.ts

--- config/plugins.ts
+++ config/plugins.ts
@@ -7,6 +7,9 @@ export default ({ env }) => ({
           accessKeyId: env("AWS_ACCESS_KEY_ID"),
           secretAccessKey: env("AWS_ACCESS_SECRET"),
           region: env("AWS_REGION"),
+          baseUrl: `https://${env("AWS_BUCKET_NAME")}.s3.${env(
+            "AWS_REGION"
+          )}.amazonaws.com`,
           params: {
             Bucket: env("AWS_BUCKET_NAME"),
           },
