1331c5d57224bbb4ffdcce597bf1086002c8b3d6
sumit-022 <lcb2021016@iiitl.ac.in>
2024-02-19 21:27:14 +0530

    changes

--- src/api/job/controllers/job.ts
+++ src/api/job/controllers/job.ts
@@ -200,6 +200,8 @@ export default factories.createCoreController("api::job.job", ({ strapi }) => ({
                 rfqNumber,
                 ENCRYPTION_KEY
               )}/${encrypt(vendor.id.toString(), ENCRYPTION_KEY)}`,
+              port: job.targetPort,
+              eta: job.vesselETA,
             }) + (ctx.request.body.mailFooter || ""),
           attachments: (() => {
             const attachment = vendors.find(
