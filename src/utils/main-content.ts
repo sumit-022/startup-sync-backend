97de2b7b148653da63e40f4911431202be156742
sumit-022 <lcb2021016@iiitl.ac.in>
2024-02-19 19:54:56 +0530

    Modified Body of RFQ Mail

--- src/utils/main-content.ts
+++ src/utils/main-content.ts
@@ -1,13 +1,13 @@
 export function getRFQMailContent({ link }: { link: string }) {
-  return `Dear Sir / Madam
-Good day,
+  return `Dear Sir / Madam<br/>
+Good day,<br/><br/>
 
-Kindly note attached requisition, please advise best price and availability of the requested parts.
+Kindly note attached requisition, please advise best price and availability of the requested parts.<br/><br/>
 
-Port: Singapore
-ETA: 16th Feb 2024
+Port: Singapore<br/>
+ETA: 16th Feb 2024<br/><br/>
 
-Please place your offer online at <a href="${link}">${link}</a>
+Please place your offer online on <a href="${link}">this link</a>.<br/>
 In case you are not able to open above link, please find attached RFQ in PDF format, kindly quote accordingly.
 We are looking forward to your offer and like to thank you for your assistance.`;
 }
