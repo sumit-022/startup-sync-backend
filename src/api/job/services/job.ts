fd827e04466183adf83fd5c2b6e7f1254e855832
chakri68 <chakridevireddy69@gmail.com>
2024-03-16 19:17:43 +0530

    Sending RFQ Details too along with Jobs

--- src/api/job/services/job.ts
+++ src/api/job/services/job.ts
@@ -67,4 +67,41 @@ export default factories.createCoreService("api::job.job", ({ strapi }) => ({
     const rfqNumber = `RFQ-${jobCode}`;
     return rfqNumber;
   },
+
+  async find(...args) {
+    // Calling the default core controller
+    const { results, pagination } = await super.find(...args);
+
+    // Check ig rfqForms are asked to be populated
+    if (!args[0].populate || !args[0].populate.includes("rfqs")) {
+      return { results, pagination };
+    }
+
+    const rfqForms = await Promise.allSettled(
+      results.map(async (job) => {
+        return await strapi.query("api::rfq.rfq").findMany({
+          where: {
+            RFQNumber: `RFQ-${job.jobCode}`,
+          },
+          populate: {
+            vendor: {
+              select: ["id", "name", "email"],
+            },
+          },
+          select: ["id", "RFQNumber", "filled"],
+        });
+      })
+    );
+
+    results.forEach((job, index) => {
+      const rfqForm = rfqForms[index];
+      if (rfqForm.status === "fulfilled") {
+        job.rfqForms = rfqForm.value;
+      } else {
+        job.rfqForms = [];
+      }
+    });
+
+    return { results, pagination };
+  },
 }));
